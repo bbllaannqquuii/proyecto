cad29507de196c1617195873ad94c373a38e2f3f
bbllaannqquuii <b_l_a_n_q_u_i_@hotmail.com>
2015-07-11 13:58:15 +0200

    11/07/2015

--- quiz/app.js
+++ quiz/app.js
@@ -1,13 +1,12 @@
+var partials=require('express-partials');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var partials = require('express-partials');
 
 var routes = require('./routes/index');
-
 
 var app = express();
 
@@ -18,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(partials());
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
