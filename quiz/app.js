e365eb51d792df8125c6d71f9e751902cecb96a1
bbllaannqquuii <b_l_a_n_q_u_i_@hotmail.com>
2015-07-11 13:40:31 +0200

    Revert "modificaciones"
    This reverts commit 5721ee33e41a92ada9067c1eec3e2753ea8e091f.

--- quiz/app.js
+++ quiz/app.js
@@ -1,12 +1,13 @@
-var partials=require('express-partials');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
+
 
 var app = express();
 
@@ -17,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(partials());
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -8,8 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-router.get('/author',quizController.author);
+router.get('/quizes/question', quizController/question);
+router.get('/quizes/answer', quizController/answer);
+router.get('/author', function(req, res) {
+  res.render('index', { title: 'author' });
+ });
 
 module.exports = router;
