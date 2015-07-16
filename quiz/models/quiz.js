f1127eb44affebed7aa2b7b4afebbe234d2244d6
bbllaannqquuii <b_l_a_n_q_u_i_@hotmail.com>
2015-07-16 21:27:08 +0200

    identificación de versión

---
+++ quiz/models/quiz.js
@@ -0,0 +1,8 @@
+//Definicion del modilo QUIZ
+
+module.exports = function (sequelize,DataTypes){
+	return sequelize.define('Quiz',
+	{ 	pregunta :  DataTypes.STRING,
+		respuesta : DataTypes.STRING,
+	});
+}		
